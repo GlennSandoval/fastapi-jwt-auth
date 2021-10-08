a30f81b614cc336a439a47312ecaf69d6dff8482
Glenn Sandoval <glenn.sandoval@gmail.com>
2021-10-08 14:50:03 +1300

    chore: remove dev testing code

--- verify_jwt.py
+++ verify_jwt.py
@@ -37,11 +37,3 @@ def verify_jwt(jwtoken: str) -> tuple[Optional[Exception], Optional[dict]]:
         error = jwt_error
 
     return error, payload
-
-
-if __name__ == "__main__":
-    e, p = verify_jwt(
-        'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjNIdkNhTFZQN3NEZVJKWURLTVNQbSJ9.eyJpc3MiOiJodHRwczovL3NhbmRvdmFsLmF1LmF1dGgwLmNvbS8iLCJzdWIiOiIxNTdnWDJoemY3bW5IblVhM1YxYW1CZHRpQVVlelU3UUBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9mYXN0YXBpLWRlbW8uaW8iLCJpYXQiOjE2MzM1Njg4MTksImV4cCI6MTYzMzY1NTIxOSwiYXpwIjoiMTU3Z1gyaHpmN21uSG5VYTNWMWFtQmR0aUFVZXpVN1EiLCJzY29wZSI6InJlYWQ6YWxsIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIiwicGVybWlzc2lvbnMiOlsicmVhZDphbGwiXX0.koMxR3WnFh3kVdyL_IdrHsacnPhomtBX0IR49mbxgIGyKe4BDD90d_PuzMm3E3eRrtrMVCGgCZuFzrfbSsQz2CLK_bY4HFCorivSl13rjd9iODbkRkYHJFRSXXYFwFX1dvoiTuu4lGHy1gO4b9Q1e7-ypG4uM7sf7r0_HWGfkNO2n4wlqldq5f2Ajok6E1Hjb3YMpv1s-HJ12jQsG9ks_wu1nCbOawyw-N9-G0GfT7kXewVGL5PmJk6mQJH22zxNVLOZXELWLnvyYOc5w4Uea7JwfUA4JAUj6FRkZkA5-nM12H31jCoMwY0mutlyvC9ZO51Zvtovyqjs5G-XHM5Yrg')
-    print("Error", e)
-    if p:
-        print("Permissions", p.get("permissions"))
